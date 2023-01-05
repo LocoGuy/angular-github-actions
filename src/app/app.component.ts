4f29fa978c4a1e46d65b6547aa3f7772ebe61333
loco <loprv@tuta.io>
2023-01-05 21:26:11 +0100

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-github-actions';
+  location: string = '';
+
+  constructor() {
+    this.location = window.location.href;
+  }
 }
